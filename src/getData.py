9844cf89e9d51db23dfb5a37326b6802498f7745
damatya <amatya.debha@gmail.com>
2015-08-26 14:01:55 -0700

    made get_data function and added miRNA and prot-dna interaction databases

--- src/getData.py
+++ src/getData.py
@@ -1,10 +1,12 @@
 import urllib
 import json
 
-with open('constants.json') as data_file:    
-    const = json.load(data_file)
+def get_data(filename):
+    with open(filename) as data_file:    
+        const = json.load(data_file)
 
-for n in range(len(const["fileURL"])):
-    data = urllib.URLopener()
-    data.retrieve(const["fileURL"][n], const["outPath"]+const["fileName"][n])
+    for n in range(len(const["fileURL"])):
+        data = urllib.URLopener()
+        data.retrieve(const["fileURL"][n], const["outPath"]+const["fileName"][n])
+       
 
